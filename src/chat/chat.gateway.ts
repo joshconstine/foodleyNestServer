a3547b0a719b305d3a9d194a4623ac0fdaa77c4e
joshua constine <joshua.constine97@gmail.com>
2023-07-23 09:45:38 -0700

    basic chat ui working

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,29 +1,41 @@
 import {
-  ConnectedSocket,
-  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+interface Chat {
+  id: number;
+  email: string;
+  text: string;
+
+  createdAt: Date;
+}
+
 @WebSocketGateway()
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
-
   onModuleInit() {
     this.server.on('connection', (socket) => {
       console.log(`Client connected: ${socket.id}`);
     });
   }
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(client: Socket, payload: Chat): Promise<void> {
+    this.server.emit('recMessage', payload);
+  }
 
-  @SubscribeMessage('send_messege')
-  onNewMessage(@MessageBody() data: string) {
-    console.log(data);
-    this.server.emit('onMessege', {
-      message: 'Received message',
-      content: data,
-    });
+  afterInit(server: Server) {
+    console.log(server);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Connected ${client.id}`);
   }
 }
