3da8db117c48606b7496d471e00863dc3df0be22
joshua constine <joshua.constine97@gmail.com>
2023-07-24 12:02:26 -0700

    removed prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RestaurantModule } from './restaurant/restaurant.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [RestaurantModule, ChatModule],
+  imports: [ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
-  }
-  getRestaurants(): string {
-    return 'Restaurants!';
   }
 }
